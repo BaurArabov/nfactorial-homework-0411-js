a97aca1caa65a9b97fd80ef2b79b613491bd105b
Baur Arabov <baur2003arabov@gmail.com>
2022-11-05 03:20:48 +0600

    Committing hw0411-js

---
+++ index.js
@@ -0,0 +1,57 @@
+// alert("Hi");
+
+// const user = {};
+
+// user.name = prompt("Type your full name > ");
+// user.age = Number(prompt("Your age? "));
+// user.sex = prompt("Male or Female? ");
+// console.log(user);
+
+
+
+// let number = +prompt('Ваше число');
+// if(number==NaN){
+//     alert("It's not a number!");
+// }
+// else{
+//     alert(number);
+// }
+
+
+
+// let a = +prompt('a?', '');
+// switch(a){
+//     case 0:
+//         alert(0);
+//         break;
+//     case 1:
+//         alert(1);
+//         break;
+//     case 2:
+//     case 3:
+//         alert("2, 3");
+//         break;
+//     default:
+//         break;;
+// }
+
+
+
+//1 способ
+// for(let i = 0; i<=100;i+=2){
+//     console.log(i);
+// }
+//2 способ
+// for(let i = 0; i<=100; i++){
+//     if(i%2==0){
+//         console.log(i);
+//     }
+// }
+
+
+
+let i = 0;
+while(i<3){
+    alert( `number ${i}!` );
+    i++;
+}
